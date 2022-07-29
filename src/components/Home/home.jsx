6f92fdf38c2e723b3fbd88530bfcf883c82f6a38
Kenechukwu <kenechukwu.ajufo@gmail.com>
2022-07-29 13:30:08 +0100

    Fixed unused import

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import { Category } from "../Category/category";
 import TrackList from "../PlaylistGrid/PlaylistGrid";
